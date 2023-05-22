daeed6beace466b27c2938b4b46fbf4c409a90de
nicofishman <74215078+nicofishman@users.noreply.github.com>
2023-05-22 19:05:24 -0300

    Added ref to the input so its focus can be controlled

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { forwardRef, useCallback, useState } from 'react';
 import {
     Animated,
     TextInput,
@@ -56,180 +56,186 @@ export type IInput = Omit<
           }
     );
 
-export const Input = ({
-    value,
-    color = 'primary_blue',
-    type = 'text',
-    onChangeText,
-    leftIcon,
-    rightIcon,
-    placeholder,
-    placeholderStyle = {},
-    style = {},
-    ...rest
-}: IInput) => {
-    const [isPassword, setIsPassword] = useState(type === 'password');
-    const [isFocused, setIsFocused] = useState(false);
-    const [showInput, setShowInput] = useState(false);
-    const [animatedIsFocused] = useState(new Animated.Value(1));
+export const Input = forwardRef(
+    (
+        {
+            value,
+            color = 'primary_blue',
+            type = 'text',
+            onChangeText,
+            leftIcon,
+            rightIcon,
+            placeholder,
+            placeholderStyle = {},
+            style = {},
+            ...rest
+        }: IInput,
+        ref: React.ForwardedRef<TextInput>
+    ) => {
+        const [isPassword, setIsPassword] = useState(type === 'password');
+        const [isFocused, setIsFocused] = useState(false);
+        const [showInput, setShowInput] = useState(false);
+        const [animatedIsFocused] = useState(new Animated.Value(1));
 
-    const labelInitialSize = style?.fontSize ?? 18;
-    const inputFontSize = placeholderStyle.fontSize ?? 12;
+        const labelInitialSize = style?.fontSize ?? 18;
+        const inputFontSize = placeholderStyle.fontSize ?? 12;
 
-    const startAnimation = useCallback(() => {
-        Animated.timing(animatedIsFocused, {
-            useNativeDriver: false,
-            toValue: showInput ? 1 : 0,
-            duration: 150
-        }).start(() => {
-            if (!showInput) {
-                setShowInput(true);
+        const startAnimation = useCallback(() => {
+            Animated.timing(animatedIsFocused, {
+                useNativeDriver: false,
+                toValue: showInput ? 1 : 0,
+                duration: 150
+            }).start(() => {
+                if (!showInput) {
+                    setShowInput(true);
+                }
+            });
+        }, [animatedIsFocused, showInput]);
+
+        const animationLabelFontSize = useCallback(() => {
+            const fontAdjust = {
+                fontSize: animatedIsFocused.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [inputFontSize, labelInitialSize]
+                })
+            };
+
+            return fontAdjust;
+        }, [animatedIsFocused, inputFontSize, labelInitialSize]);
+
+        function handleFocus() {
+            if (value.length > 0) {
+                return;
             }
-        });
-    }, [animatedIsFocused, showInput]);
-
-    const animationLabelFontSize = useCallback(() => {
-        const fontAdjust = {
-            fontSize: animatedIsFocused.interpolate({
-                inputRange: [0, 1],
-                outputRange: [inputFontSize, labelInitialSize]
-            })
-        };
-
-        return fontAdjust;
-    }, [animatedIsFocused, inputFontSize, labelInitialSize]);
-
-    function handleFocus() {
-        if (value.length > 0) {
-            return;
+            setIsFocused(true);
+            if (!showInput) {
+                startAnimation();
+            }
         }
-        setIsFocused(true);
-        if (!showInput) {
-            startAnimation();
+
+        function handleBlur() {
+            if (value.length > 0) {
+                return;
+            }
+            setIsFocused(false);
+            if (!value) {
+                setShowInput(false);
+
+                startAnimation();
+            }
         }
-    }
 
-    function handleBlur() {
-        if (value.length > 0) {
-            return;
+        if (leftIcon?.props.size && leftIcon.props.size > 40) {
+            throw new Error(
+                'The size of the leftIcon must be less than or equal to 40'
+            );
         }
-        setIsFocused(false);
-        if (!value) {
-            setShowInput(false);
-
-            startAnimation();
+        if (rightIcon?.props.size && rightIcon.props.size > 40) {
+            throw new Error(
+                'The size of the rightIcon must be less than or equal to 40'
+            );
         }
-    }
 
-    if (leftIcon?.props.size && leftIcon.props.size > 40) {
-        throw new Error(
-            'The size of the leftIcon must be less than or equal to 40'
-        );
-    }
-    if (rightIcon?.props.size && rightIcon.props.size > 40) {
-        throw new Error(
-            'The size of the rightIcon must be less than or equal to 40'
-        );
-    }
-
-    return (
-        <View
-            style={{
-                width: '100%',
-                position: 'relative',
-                height: 40,
-                marginVertical: 10
-            }}
-        >
-            <Animated.Text
-                style={[
-                    {
-                        position: 'absolute',
-                        top: 10,
-                        left:
-                            leftIcon && !isFocused && value.length === 0
+        return (
+            <View
+                style={{
+                    width: '100%',
+                    position: 'relative',
+                    height: 40,
+                    marginVertical: 10
+                }}
+            >
+                <Animated.Text
+                    style={[
+                        {
+                            position: 'absolute',
+                            top: 10,
+                            left:
+                                leftIcon && !isFocused && value.length === 0
+                                    ? (leftIcon.props.size ?? 10) + 10
+                                    : 0,
+                            width: '100%',
+                            color: placeholderStyle.color ?? '#939393',
+                            textTransform: 'uppercase',
+                            height: '100%',
+                            fontSize: 22,
+                            fontFamily: 'Nunito_600SemiBold',
+                            transform: [
+                                {
+                                    translateY: isFocused ? -25 : 0
+                                }
+                            ]
+                        },
+                        animationLabelFontSize()
+                    ]}
+                >
+                    {placeholder}
+                </Animated.Text>
+                {leftIcon && (
+                    <View
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            height: '100%',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}
+                    >
+                        {leftIcon}
+                    </View>
+                )}
+                <TextInput
+                    ref={ref}
+                    keyboardType={type === 'number' ? 'numeric' : 'default'}
+                    secureTextEntry={isPassword}
+                    style={[
+                        {
+                            borderBottomColor: COLORS[color],
+                            borderBottomWidth: isFocused ? 2 : 1,
+                            fontFamily: 'Nunito_600SemiBold',
+                            fontSize: 22,
+                            height: '100%',
+                            paddingLeft: leftIcon
                                 ? (leftIcon.props.size ?? 10) + 10
                                 : 0,
-                        width: '100%',
-                        color: placeholderStyle.color ?? '#939393',
-                        textTransform: 'uppercase',
-                        height: '100%',
-                        fontSize: 22,
-                        fontFamily: 'Nunito_600SemiBold',
-                        transform: [
-                            {
-                                translateY: isFocused ? -25 : 0
-                            }
-                        ]
-                    },
-                    animationLabelFontSize()
-                ]}
-            >
-                {placeholder}
-            </Animated.Text>
-            {leftIcon && (
-                <View
-                    style={{
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        height: '100%',
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                    }}
-                >
-                    {leftIcon}
-                </View>
-            )}
-            <TextInput
-                keyboardType={type === 'number' ? 'numeric' : 'default'}
-                secureTextEntry={isPassword}
-                style={[
-                    {
-                        borderBottomColor: COLORS[color],
-                        borderBottomWidth: isFocused ? 2 : 1,
-                        fontFamily: 'Nunito_600SemiBold',
-                        fontSize: 22,
-                        height: '100%',
-                        paddingLeft: leftIcon
-                            ? (leftIcon.props.size ?? 10) + 10
-                            : 0,
-                        paddingRight: rightIcon
-                            ? (rightIcon.props.size ?? 10) + 10
-                            : 0
-                    },
-                    style
-                ]}
-                value={value}
-                onBlur={handleBlur}
-                onChangeText={(text) => onChangeText(text)}
-                onFocus={handleFocus}
-                {...rest}
-            />
-            {(rightIcon || type === 'password') && (
-                <View
-                    style={{
-                        position: 'absolute',
-                        top: 0,
-                        right: 0,
-                        height: '100%',
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                    }}
-                >
-                    {type === 'password' ? (
-                        <Icon
-                            color='black'
-                            library='Entypo'
-                            name={isPassword ? 'eye-with-line' : 'eye'}
-                            size={25}
-                            onPress={() => setIsPassword(!isPassword)}
-                        />
-                    ) : (
-                        rightIcon
-                    )}
-                </View>
-            )}
-        </View>
-    );
-};
+                            paddingRight: rightIcon
+                                ? (rightIcon.props.size ?? 10) + 10
+                                : 0
+                        },
+                        style
+                    ]}
+                    value={value}
+                    onBlur={handleBlur}
+                    onChangeText={(text) => onChangeText(text)}
+                    onFocus={handleFocus}
+                    {...rest}
+                />
+                {(rightIcon || type === 'password') && (
+                    <View
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            right: 0,
+                            height: '100%',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}
+                    >
+                        {type === 'password' ? (
+                            <Icon
+                                color='black'
+                                library='Entypo'
+                                name={isPassword ? 'eye-with-line' : 'eye'}
+                                size={25}
+                                onPress={() => setIsPassword(!isPassword)}
+                            />
+                        ) : (
+                            rightIcon
+                        )}
+                    </View>
+                )}
+            </View>
+        );
+    }
+);
